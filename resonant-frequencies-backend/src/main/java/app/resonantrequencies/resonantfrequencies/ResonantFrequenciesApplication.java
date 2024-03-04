f3cee194aa20aca7de6734b44cab28d7f3610647
Jamie Porche <daisy@Jamies-MacBook-Pro-2.local>
2024-03-03 20:15:44 -0700

    initial commit

---
+++ resonant-frequencies-backend/src/main/java/app/resonantrequencies/resonantfrequencies/ResonantFrequenciesApplication.java
@@ -0,0 +1,13 @@
+package app.resonantrequencies.resonantfrequencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResonantFrequenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResonantFrequenciesApplication.class, args);
+	}
+
+}
---
+++ resonant-frequencies-backend/src/test/java/app/resonantrequencies/resonantfrequencies/ResonantFrequenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package app.resonantrequencies.resonantfrequencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResonantFrequenciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
